18986aa72439a3289b13583d7d14d9ea0736af1f
prontoSoCorso <152395761+prontoSoCorso@users.noreply.github.com>
2024-06-01 10:26:34 +0200

    Create Folders and Config

---
+++ config.py
@@ -0,0 +1,48 @@
+''' Configuration file for the project'''
+
+class user_paths:
+    # Per computer fisso
+    path_BlastoData = "/home/giovanna/Documents/Data/BlastoData/"
+    
+    #Per computer portatile
+    #path_BlastoData = "C:/Users/loren/Documents/Data/BlastoData/"
+
+
+class Config_00_createCsv:
+    
+
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
